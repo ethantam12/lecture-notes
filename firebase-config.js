24a1b5488a58b50aff5a723505241bfc3ca9435b
ethantam12 <ethantam1212@gmail.com>
2026-09-15 15:34:07 -0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyDG9XhGY53TlyiiX1esz3NuxwTCJucwwNM",
+  authDomain: "lecture-recording-app-a0003.firebaseapp.com",
+  projectId: "lecture-recording-app-a0003",
+  storageBucket: "lecture-recording-app-a0003.firebasestorage.app",
+  messagingSenderId: "498560874783",
+  appId: "1:498560874783:web:b8b99fdc3c75edbb39982a"
+};
---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.supabaseConfig = {
+  url: "https://qhgeksgjyyxxdakbdcpa.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoZ2Vrc2dqeXl4eGRha2JkY3BhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1MDAxOTksImV4cCI6MjEwNTA3NjE5OX0.711gi6JnGl1uS8Zu-FixPOO7KYD4S_bn80TlwK_c8tg"
+};
